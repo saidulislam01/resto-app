5b540f27f0737deb25ee69cd65083ac10550e698
saidulislam01 <saidulislam7733@gmail.com>
2022-12-11 00:27:56 +0600

    admin auth has been added

---
+++ database/seeders/Admin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class Admin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678') ,
+            'remember_token' => Str::random(10),
+            'is_admin' => 1,
+        ]);
+
+    }
+}
